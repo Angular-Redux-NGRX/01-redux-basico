ae2ad4208c7a9f51895d253eaea71d06c428dd9f
Juan David Mendez Campuzano <juandavidmendezcampuzano@gmail.com>
2021-08-18 20:09:37 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 01-redux-basico
---
+++ app-2.ts
@@ -0,0 +1,14 @@
+import { Action } from "./ngrx-fake/ngrx";
+import { incrementadorAction, decrementadorAction, multiplicarAction, dividirAction, resetAction } from './contador/contador.actions';
+import { contadorReducer } from './contador/contador.reducer';
+
+
+console.log(contadorReducer(10, incrementadorAction));
+
+console.log( contadorReducer(10, decrementadorAction));
+
+console.log( contadorReducer(10, multiplicarAction));
+
+console.log( contadorReducer(10, dividirAction));
+
+console.log( contadorReducer(10, resetAction));
